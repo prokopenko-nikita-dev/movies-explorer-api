cd2be05cfba0e4f4480884590c592cf3354da592
Prokopenko Nikita <117819655+prokopenko-nikita-dev@users.noreply.github.com>
2023-08-20 22:15:28 +0300

    edit routes app.js

--- app.js
+++ app.js
@@ -2,37 +2,42 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const limit = require('./middlewares/limit');
+
 const NotFoundError = require('./errors/NotFoundError');
 const errorHandler = require('./middlewares/error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const limit = require('./middlewares/limit');
 
 const { PORT = 3000, BASE_PATH } = process.env;
 
+const corsOptions1 = {
+  origin: '',
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
 const app = express();
+app.use(cors(corsOptions1));
 
 app.use(requestLogger);
-app.use('/', limit);
+app.use(limit);
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// CORS
-app.use(cors);
+require('./routes')(app);
 
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
-
-// все роуты в index.js
-require('./routes')(app);
 
 app.use('/*', () => {
   throw new NotFoundError('Страница не найдена');
